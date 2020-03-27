631c684095c73a55cc25f2bb5ad408906b05760d
Tony KHAOV <tony.khaov@gmail.com>
2020-03-27 20:46:21 +0100

    :building_construction: Making architectural changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import EnterStore from "./components/EnterStore";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Store from "./components/Store";
+import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
-  return <div></div>;
+  const [username, setUsername] = useState(null);
+  return (
+    <Router>
+      <div>
+        <Header username={username} />
+        <Switch>
+          {
+            // ENTERSTORE PAGE
+          }
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <EnterStore setUsername={setUsername} {...props} />
+            )}
+          />
+
+          {
+            // BUY IN THE SHOP PAGE
+          }
+          <Route
+            exact
+            path="/store/:username"
+            render={props => <Store {...props} username={username} />}
+          />
+
+          {
+            // EDIT THE SHOP PAGE
+          }
+          <Route
+            exact
+            path="/store/:username/edit"
+            render={props => <h1>J'édite de {props.match.params.username}</h1>}
+          />
+
+          {
+            // OUT OF PATH
+          }
+          <Route>
+            <p>ERREUR</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
