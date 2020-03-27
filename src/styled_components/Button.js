d96b666951a671bcb1a15b77d04fc5e90b0a149c
Tony KHAOV <tony.khaov@gmail.com>
2020-03-27 20:45:15 +0100

    :building_construction: Making architectural changes

---
+++ src/styled_components/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 10px 16px;
+  font-size: 24px;
+  border-radius: 8px;
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  &:hover {
+    background-color: hsl(136, 0%, 99%);
+  }
+`;
---
+++ src/styled_components/Input.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  padding: 6px 8px;
+  font-size: 24px;
+  border: 1px solid hsl(136, 0%, 80%);
+  border-radius: 6px;
+`;
