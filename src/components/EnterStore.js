2ea3be03b9514185e71897f9eb17b84000b0e99d
Tony KHAOV <tony.khaov@gmail.com>
2020-03-28 20:01:53 +0100

    :lipstick: Update the UI

--- src/components/EnterStore.js
+++ src/components/EnterStore.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
-import { Button, Input } from "../styled_components";
+import { Button, Input, Card } from "../styled_components";
+import { black, grey, pink, double_box_shadow } from "../Utilities";
 
 function EnterStore(props) {
   const inputEl = useRef(null);
@@ -13,43 +14,87 @@ function EnterStore(props) {
   };
 
   return (
-    <FormWrapper onSubmit={handleSubmit}>
-      <h1>Connexion</h1>
-      <p>Entre ton nom d'utilisateur</p>
-      <InputForm
-        ref={inputEl}
-        type="text"
-        placeholder="Nom d'utilisateur"
-        required
-      />
-      <ButtonForm type="submit">Valider</ButtonForm>
-    </FormWrapper>
+    <CardWrapper>
+      <FormWrapper onSubmit={handleSubmit}>
+        <TitleForm>Connexion</TitleForm>
+        <Hr />
+        <DescriptionForm>Entre ton nom d'utilisateur</DescriptionForm>
+        <InputForm
+          ref={inputEl}
+          type="text"
+          placeholder="Nom d'utilisateur"
+          required
+        />
+        <ButtonForm type="submit">Valider</ButtonForm>
+        <Author href="http://github.com/tonykhaov">par Tony KHAOV</Author>
+      </FormWrapper>
+    </CardWrapper>
   );
 }
+
+const CardWrapper = styled(Card)`
+  max-width: 480px;
+  margin: auto;
+  margin-top: 32px;
+  padding: 32px;
+  box-shadow: ${double_box_shadow[4]};
+`;
 
 const FormWrapper = styled.form`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin: 48px;
-  font-size: 32px;
-
-  p {
-    font-size: 24px;
-  }
 `;
+
+const TitleForm = styled.h1``;
+
+const Hr = styled.hr`
+  background-color: ${pink[600]};
+  height: 2px;
+  border-radius: 2px;
+  border: none;
+  width: 100%;
+  margin: 8px;
+`;
+
+const DescriptionForm = styled.p`
+  color: ${grey[600]};
+  font-weight: 100;
+`;
+
 const InputForm = styled(Input)`
-  margin: 24px;
+  margin: 8px;
+  padding: 10px 16px;
+  border: 1px solid ${black[400]};
+  width: 100%;
 `;
 
 const ButtonForm = styled(Button)`
-  background-color: hsl(23, 100%, 54%);
+  background-color: ${pink[600]};
+  font-size: 16px;
+  padding: 10px 12px;
   color: white;
   font-weight: 600;
-  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: none;
+  margin-top: 12px;
+  width: 100%;
   &:hover {
-    background-color: hsl(23, 87%, 53%);
+    background-color: ${pink[500]};
+  }
+`;
+
+const Author = styled.a`
+  position: absolute;
+  left: 12px;
+  bottom: 8px;
+  text-align: center;
+  font-size: 14px;
+  font-family: Poppins;
+  font-weight: 300;
+  color: white;
+  display: flex;
+  letter-spacing: -0.01rem;
   }
 `;
 
