9e814da909a84c02ec161124d599cd7277c0febe
Tony KHAOV <tony.khaov@gmail.com>
2020-03-28 20:01:41 +0100

    :construction_worker: Add Utilities

---
+++ src/Utilities/shadow.js
@@ -0,0 +1,15 @@
+export const box_shadow = [
+  "0 1px 3px rgba(0,0,0,0.2)",
+  "0 4px 6px rgba(0,0,0,0.2)",
+  "0 5px 15px rgba(0,0,0,0.2)",
+  "0 10px 24px rgba(0,0,0,0.2)",
+  "0 15px 35px rgba(0,0,0,0.2)"
+];
+
+export const double_box_shadow = [
+  "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+  "0 3px 6px rgba(0,0,0,0.15), 0 2px 4px rgba(0,0,0,0.12)",
+  "0 10px 20px rgba(0,0,0,0.15), 0 3px 6px rgba(0,0,0,0.10)",
+  "0 15px 25px rgba(0,0,0,0.15), 0 5px 10px rgba(0,0,0,0.05)",
+  "0 20px 40px rgba(0,0,0,0.2)"
+];
