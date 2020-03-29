5d15e77cdaeadd57cd6971f7941d3dd98204e1ed
Tony KHAOV <tony.khaov@gmail.com>
2020-03-29 19:04:28 +0200

    :sparkles: Add original images

---
+++ src/sample-images.js
@@ -0,0 +1,32 @@
+export const SAMPLE_IMAGES = {
+  image1: {
+    name: "#LETSGOHEALTHY",
+    price: "300",
+    status: "unavailable"
+  },
+  image2: {
+    name: "Voiture orange.",
+    price: "200",
+    status: "available"
+  },
+  image3: {
+    name: "Samedi soir en ville.",
+    price: "400",
+    status: "available"
+  },
+  image4: {
+    name: "Cabanon sur un lac.",
+    price: "100",
+    status: "available"
+  },
+  image5: {
+    name: "Bébé !!!",
+    price: "800",
+    status: "available"
+  },
+  image6: {
+    name: "Mona Lisa.",
+    price: "600",
+    status: "available"
+  }
+};
