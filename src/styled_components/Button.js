3e7774bc5b3a70d6c03b744ae22cb8a43d5d02d2
Tony KHAOV <tony.khaov@gmail.com>
2020-03-29 19:05:03 +0200

    :art: Format the code

--- src/styled_components/Button.js
+++ src/styled_components/Button.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
-import { black } from "../Utilities";
+import { double_box_shadow } from "../Utilities";
 
 export const Button = styled.button`
   padding: 10px 16px;
   border: 0;
   border-radius: 8px;
-  box-shadow: 2px 2px 6px ${black[200]};
+  box-shadow: ${double_box_shadow[1]};
   cursor: pointer;
+  &:active {
+    box-shadow: ${double_box_shadow[0]};
+  }
 `;
--- src/styled_components/Input.js
+++ src/styled_components/Input.js
@@ -3,10 +3,14 @@ import { grey } from "../Utilities";
 
 export const Input = styled.input`
   padding: 6px 8px;
-  border: 1px solid ${grey[400]};
+  border: 1px solid ${grey.warm[300]};
+  background-color: ${grey.blue[100]};
   font-size: 16px;
   border-radius: 6px;
   &::placeholder {
-    color: ${grey[700]};
+    color: ${grey.warm[800]};
+  }
+  &:focus {
+    background-color: white;
   }
 `;
