43c88fb1fa56b5edcbdbe3bf5b54bd5582f979a3
Tony KHAOV <tony.khaov@gmail.com>
2020-03-30 18:12:53 +0200

    :sparkles: Update original states

--- src/sample-images.js
+++ src/sample-images.js
@@ -1,32 +1,51 @@
 export const SAMPLE_IMAGES = {
   image1: {
     name: "#LETSGOHEALTHY",
+    author: "Pierre",
     price: "300",
+    url: "/images/image1.jpg",
     status: "unavailable"
   },
   image2: {
     name: "Voiture orange.",
+    author: "Emilie",
     price: "200",
+    url: "/images/image2.jpg",
     status: "available"
   },
   image3: {
-    name: "Samedi soir en ville.",
+    name: "Samedi soir.",
+    author: "Pierre",
     price: "400",
+    url: "/images/image3.jpg",
     status: "available"
   },
   image4: {
-    name: "Cabanon sur un lac.",
+    name: "Lac.",
+    author: "Robin",
     price: "100",
+    url: "/images/image4.jpg",
     status: "available"
   },
   image5: {
     name: "Bébé !!!",
+    author: "Admin",
     price: "800",
+    url: "/images/image5.jpg",
     status: "available"
   },
   image6: {
     name: "Mona Lisa.",
+    author: "Léonard De Vinci",
     price: "600",
+    url: "/images/image6.jpg",
+    status: "available"
+  },
+  image7: {
+    name: "Bichon Maltais",
+    author: "Maman",
+    price: "9900",
+    url: "/images/image7.jpg",
     status: "available"
   }
 };
