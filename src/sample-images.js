253fa30ba93f1fbe981ce1070b8e35d67c98a96a
Tony KHAOV <tony.khaov@gmail.com>
2020-04-03 19:30:12 +0200

    :rocket: Deploy & Update images

--- src/sample-images.js
+++ src/sample-images.js
@@ -13,32 +13,11 @@ export const SAMPLE_IMAGES = {
     url: "/images/image2.jpg",
     deletable: false,
   },
-  image1585755410762: {
-    name: "Baby!!!",
-    author: "Admin",
-    price: "800",
-    url: "/images/image3.jpg",
-    deletable: false,
-  },
   image1585755410764: {
     name: "Come on SSBU",
     author: "Tony KHAOV",
     price: "300",
     url: "https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-    deletable: false,
-  },
-  image1585755410765: {
-    name: "Mona Lisa.",
-    author: "Leonard De Vinci",
-    price: "600",
-    url: "/images/image6.jpg",
-    deletable: false,
-  },
-  image1585755410766: {
-    name: "Bikepack 2021.",
-    author: "Tony KHAOV",
-    price: "690",
-    url: "https://images.unsplash.com/photo-1511994298241-608e28f14fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     deletable: false,
   }
 };
